ab9e8ea9f709bc7953d4642d1d7eae4bceed9a94
Ederaldo Souza <84224107+EderSouza93@users.noreply.github.com>
2025-02-15 17:28:19 -0300

    feat: services controllers users

--- index.js
+++ index.js
@@ -1,24 +1,14 @@
 import express from 'express';
+import userRoutes from './src/routes/user.routes.js'
 const app = express();
 
 app.use(express.json());
-
-const users = []
-
-// METHOD => GET, POST, PUT/PATCH, DELETE - API RESTful
-// NAME => / - sempre no plural
-// Callback functions => Onde excutamos o backend (lógica, regra de negócio)
-
-app.post('/users', (req, res) => {
-    const body = req.body
-    users.push(body)
-    res.status(201).send("Usuário criado com sucesso!")
-  });
-
-app.get("/users", (req, res) => {
-  res.send({message: "Esses são os users", users})
-})
+app.use(userRoutes);
 
 
-app.listen(3000, () => console.log("Server is running on port 3000"));
+
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000")
+});
 
---
+++ src/controller/user.controllers.js
@@ -0,0 +1,19 @@
+import userService from "../service/user.service.js";
+
+async function createUserController(req, res) {
+    const newUser = req.body;
+
+    try {
+        const user = userService.createUserService(newUser);
+        res.status(201).send({user})
+    } catch (err) {
+        return res.status(400).send(e.message);
+    }
+
+}
+
+
+
+export default {
+    createUserController
+}
