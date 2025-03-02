638feed8c12fee95fc3580d13cd272262c8b82a1
Ederaldo Souza <84224107+EderSouza93@users.noreply.github.com>
2025-03-01 23:10:56 -0300

    fix: name of database

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,11 @@
-import sqlite3 from 'sqlite3';
+import sqlite3 from "sqlite3";
 
+const db = new sqlite3.Database("Library_db.sqlite", (err) => {
+  if (err) {
+    console.error("Error ao conectar ao banco de dados:", err.message);
+  } else {
+    console.log("Conectado com sucesso ao banco de Dados SQLite.");
+  }
+});
 
-const db = new sqlite3.Database('Library_db.squiite', (err) => {
-    if (err) {
-        console.error('Error ao conectar ao banco de dados:', err.message);
-        } else {
-            console.log('Conectado com sucesso ao banco de Dados SQLite.')
-        }
-})
-
-export default db
+export default db;
